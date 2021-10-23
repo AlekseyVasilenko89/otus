cdd8d15ff0d70f5e0c33f7516137878ad7773e9a
AlekseyVasilenko89 <53444650+AlekseyVasilenko89@users.noreply.github.com>
2021-10-23 16:32:10 +0300

    hw01-gradle

--- L01-gradle/src/main/java/ru/otus/App.java
+++ L01-gradle/src/main/java/ru/otus/App.java
@@ -22,13 +22,7 @@ import java.util.List;
  */
 public class App {
     public static void main(String... args) {
-        List<Integer> example = new ArrayList<>();
-        int min = 0;
-        int max = 100;
-        for (int i = min; i < max; i++) {
-            example.add(i);
-        }
-
-        System.out.println(Lists.reverse(example));
+        System.out.println(HelloOtus.foo(null, "Not null two"));
+        System.out.println(HelloOtus.foo("Not null one", "Not null two"));
     }
 }
